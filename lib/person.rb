ff8595f0b5154c5b52712eb79bbe0033e84c35fd
Sergio <kurisergio777777@gmail.com>
2022-05-23 16:22:28 -0400

    commit tree 6cb5a83c56f84b86ff12cba99bc813f7d0d6cda7

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,14 @@
 class Person
-  # your code here
+
+  hash = {:name, :birthday, :hair_color, :eye_color, :height,
+  :weight, :handed, :complexion, :t_shirt_size,
+  :wrist_size, :glove_size, :pant_length, :pant_width}
+
+  def initialized(hash)
+    hash.each  do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=#{value}")
+  end 
+
+
 end
